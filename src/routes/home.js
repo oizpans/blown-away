e035d5a77a0f98a1ffdaff37d08690d0aa17af77
mike <msonderhoj@gmail.com>
2018-11-29 10:59:38 +0800

    Added mongo

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,18 @@
+const mongoClient = require('mongodb').MongoClient;
 
 
 const index = async (req, res, next) => {
 
+  let mongo = null;
+  try {
+    await mongoClient.connect('mongodb://mongo:27017/test',  { useNewUrlParser: true } );
+    mongo = true;
+  } catch (error) {
+    mongo = false;
+  }
+
   return res.render('home', {
+    mongo: mongo,
   });
 
 };
