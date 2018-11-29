4b6cea28c24564a1d5df7ab9d5e937d479f34582
mike <msonderhoj@gmail.com>
2018-11-29 11:01:58 +0800

    Removed nats

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
-const { Client } = require('feathers-mq');
 const { hbs, notFound, errorHandler  } = require('./middleware');
 const { home } = require('./routes');
 
@@ -9,7 +8,6 @@ app.set('trust proxy', true);
 app.set('name', 'blown-away');
 app.use('/', express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
-app.configure(Client({ timeout: 10000 }));
 app.configure(hbs);
 app.get('/', home.index);
 app.use(notFound);
