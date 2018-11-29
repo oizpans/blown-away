2d1cf8e26a6e50dce3a1aa6cebf12280b2c2acdb
mike <msonderhoj@gmail.com>
2018-11-29 11:27:58 +0800

    Added redis and nats

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,16 +3,38 @@ const mongoClient = require('mongodb').MongoClient;
 
 const index = async (req, res, next) => {
 
-  let mongo = null;
+  const result = {
+    mongo: null,
+    redis: null,
+    nats: null,
+  };
+
   try {
     await mongoClient.connect('mongodb://mongo:27017/test',  { useNewUrlParser: true } );
-    mongo = true;
+    result.mongo = true;
   } catch (error) {
-    mongo = false;
+    console.log(error);
+  }
+
+  try {
+    const Redis = require('redis');
+    const client = Redis.createClient();
+    result.redis = true;
+  } catch (error) {
+    console.log(error);
+  }
+
+
+  try {
+    var NATS = require('nats');
+    var nats = NATS.connect();
+    result.nats = true;
+  } catch (error) {
+    console.log(error);
   }
 
   return res.render('home', {
-    mongo: mongo,
+    result: result,
   });
 
 };
